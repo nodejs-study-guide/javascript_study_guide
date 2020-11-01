ba9edc32991e0676b0cbe9fb9d53a4b1e9439f5d
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-01 20:22:38 +0000

    Create spread_operators.js

---
+++ spread_operators.js
@@ -0,0 +1,13 @@
+// spread operators spreads expands an array into individual arguments before 
+// feeding it into a function. It's kind of like how a "rest parameter" works 
+// but in reverse. 
+
+function greet(person1, person2) {
+  console.log('Hello ' + person1 + ' and ' + person2 );
+}
+
+let names = ['John', 'Mary'];
+
+// notice we pass in '...' which kinda explodes the array into individual 
+// comma seperated arguments before feeding them into the 'greet' function. 
+greet(...names); // Hello John and Mary
