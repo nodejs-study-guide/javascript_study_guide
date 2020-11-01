5cb98cb62f368fc27da335ff3edc20de2ebaa744
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-01 10:49:57 +0000

    Update closures.js

--- closures.js
+++ closures.js
@@ -1,1 +1,11 @@
-test
+let greetings = (function() {
+  let message = "hello";
+  let getMessage = function() {
+    return message;
+  };
+  return {
+    getFinalMessage: getMessage,
+  };
+})();
+
+console.log(greetings.getFinalMessage())
