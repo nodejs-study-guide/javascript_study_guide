75696b2046997f0e35e7c58748196f6e220d2dea
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-01 18:37:44 +0000

    Create the_call_method.js

---
+++ the_call_method.js
@@ -0,0 +1,26 @@
+/*
+A variables that's storing a function, has a number of builtin methods already avaiable. 
+One of which is the call() method. 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
+
+This method lets you feed inject an object {struct} into a function, and that object exists
+in the form of a "this" object. 
+*/
+
+let person1 = {
+  name: 'John',
+  age: 22,
+}
+
+let person2 = {
+  name: 'Mary',
+  age: 34,
+}
+
+let greetings = function(){
+  console.log('Hello ' + this.name);
+};
+
+greetings.call(person1); // Hello John
+greetings.call(person2); // Hello Mary
