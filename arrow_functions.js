cee1c65488f31104fb5f150f65c3f925f6692714
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-01 13:38:59 +0000

    Update arrow_functions.js

--- arrow_functions.js
+++ arrow_functions.js
@@ -1,4 +1,4 @@
-// A regular function
+// A regular anonymous-function
 
 let greetings = function() {
   return 'Hello World';
@@ -10,9 +10,76 @@ console.log(message);      // Hello World
 
 
 // arrow function equivalent (just the first line is different)
+// the empty bracket means that there's no input parameters. 
+
 let greetings = () => {
   return 'Hello World';
 };
 
 let message = greetings();
 console.log(message);      // Hello World
+
+
+// Since the code block only has one line, you can make it even shorter
+// by removing the curly braces and return key word
+
+let greetings = () =>  'Hello World';
+
+let message = greetings();
+console.log(message);      // Hello World
+
+
+
+// Now lets take a look at converting a regular-function (which has one paraemter)
+// into an arrow function
+let greet = function greetings(name) {
+  return 'Hello ' + name
+}
+
+let message = greet('John');
+console.log(message);        // Hello John 
+
+
+// now the arrow-function equivalent
+// notice we no longer needed to create the "greet" variable
+let greetings = (name) => {
+  return 'Hello ' + name
+}
+
+let message = greetings('John');
+console.log(message);        // Hello John 
+
+
+
+// Also when you have just one input parameter, 
+// then you can also get rid of the round brackets as well
+
+// now the arrow-function equivalent
+let greetings = name => {
+  return 'Hello ' + name
+}
+
+let message = greetings('John');
+console.log(message);        // Hello John 
+
+
+// arrow functions with 2 or more input parameters. 
+// here's the regular function
+function sum(num1, num2) {
+  return num1 + num2;
+}
+let output = sum(5,10)
+console.log(output);        // 15
+
+
+// arrow-function equivalent
+let sum = (num1, num2) => {
+  return num1 + num2;
+}
+let output = sum(7,10)
+console.log(output);        // 17
+
+
+
+
+
