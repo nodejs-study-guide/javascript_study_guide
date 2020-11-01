b04e233e4fca047e21110dbd6bae57f6906daafe
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-01 20:15:19 +0000

    Update rest_parameters.js

--- rest_parameters.js
+++ rest_parameters.js
@@ -8,3 +8,15 @@ greetings('Mary', 'John', 'James')
 // Hi Mary
 // Hi John
 // Hi James
+
+
+// Rest Parameters are the same thing as golang's variadic function. It even uses the same dotdotdot notation, "..."
+
+let greetings = function greet(message, ...names){
+  names.forEach(name => console.log(message + ' ' + name));
+}
+
+greetings('Welcome', 'Mary', 'John', 'James')
+// Welcome Mary
+// Welcome John
+// Welcome James
