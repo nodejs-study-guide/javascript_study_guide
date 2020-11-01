e545e194071af120aeb2113c8a89ba16713fc63c
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-01 20:25:28 +0000

    Update spread_operators.js

--- spread_operators.js
+++ spread_operators.js
@@ -11,3 +11,12 @@ let names = ['John', 'Mary'];
 // notice we pass in '...' which kinda explodes the array into individual 
 // comma seperated arguments before feeding them into the 'greet' function. 
 greet(...names); // Hello John and Mary
+
+
+// It can also split a string variable too. 
+function greet(person1, person2) {
+  console.log('Hello ' + person1 + ' and ' + person2 );
+}
+
+let letters = 'ab';
+greet(...letters); // Hello a and b
