e196a0eca60484f848a9ee833105a333ccd8ac5f
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-07 22:41:40 +0000

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,23 +1,70 @@
-function printAndRespond (message, callback) {
-  console.log('Message is : ' + message)
-  // The setTimeout is a built in function
-  // that accepts a callback function 
-  // followed by milliseconds delay before executing that function
-  // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals
 
-  setTimeout(callback, 3000) // this is fired and forgotten
-}
 
-function myCallback () {
-  console.log('callback called')
+
+function printAndRespond (message, delay) {
+    let apiResponse = function() {
+      console.log('Message is : ' + message)
+    }
+    setTimeout(apiResponse, delay)
 }
 console.log('starting')
-printAndRespond('Hi Sher', myCallback)
+printAndRespond('first do 1', 5000)
+printAndRespond('then do 2', 1000)
+printAndRespond('then do 3', 1000)
+
+
 console.log('finished')
 
-// This out outputs
+/*
+In this example we, used teh builtin setTimeout() function
+as a way to mimic api responses, which can be slow 
+due to network speed issues. 
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals
+The first parameter specifies what function to run, and the
+second parameter sets how how many milliseconds to wait before
+running the function. 
+
+
+The above outputs:
 
 // starting
-// Message is : Hi Sher
 // finished
-// callback called     (this gets outputted after 3 seconds)
+// Message is : then do 2
+// Message is : then do 3
+// Message is : first do 1
+
+The ordering looks wrong here. 
+
+So how do we fix it?
+
+
+One option is to set up callbacks, and nested arrow functions
+
+*/
+
+
+function printAndRespond (message, delay, callback) {
+  console.log('Message is: ' + message)
+  setTimeout(callback, delay)
+}
+console.log('starting')
+printAndRespond('first do 1', 5000, () => {
+  printAndRespond('then do 2', 1000, () => {
+    printAndRespond('then do 3', 1000, () => {})
+  })
+})
+console.log('finished')
+
+
+/*
+This outputs:
+
+// starting
+// Message is : first do 1
+// finished
+// Message is : then do 2
+// Message is : then do 3
+// no more requests
+
+*/
+
