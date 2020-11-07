356680322ac2f507b667d5afcc42c7bab31b4aa9
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2020-11-07 21:38:17 +0000

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,22 @@
+function printAndRespond (message, callback) {
+  console.log('Message is : ' + message)
+  // The setTimeout is a built in function
+  // that accepts a callback function 
+  // followed by milliseconds delay before executing that function
+  // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals
+  setTimeout(callback, 3000) //
+}
+
+function myCallback () {
+  console.log('callback called')
+}
+console.log('starting')
+printAndRespond('Hi Sher', myCallback)
+console.log('finished')
+
+// This out outputs
+
+// starting
+// Message is : Hi Sher
+// finished
+// callback called     (this gets outputted after 3 seconds)
