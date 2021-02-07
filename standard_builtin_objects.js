9cc7ba07cf0731841d84e60561aedbbaf296cf22
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2021-02-07 17:53:57 +0000

    Update standard_builtin_objects.js

--- standard_builtin_objects.js
+++ standard_builtin_objects.js
@@ -9,18 +9,31 @@ person = {
 // e.g the "Math" object:
 
 console.log(typeof Math)     // object
+
+// here we're accessing one of Math object's property
 console.log(Math.PI)         // 3.141 
+
+// here we're accessing one of it's methods 
 console.log(Math.sqrt(16))   // 44
 
-// Some of these builtin objects are actually just builtin fucntions
+
+
+
+
+
+// Some of these builtin objects are actually just builtin functions, e.g. "Date"
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
 console.log(typeof Date)  // function
 
+// you can call some special methods called "static methods" directly on this builtin function 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#static_methods
+
+console.log(Date.now())  // 1612720322842
+
+
+// However to use the 'protoytpe' functions you have to create a "Date" object and apply the function to that object
 y = new Date() 
-
-
-
 
 console.log(typeof y)    // object
 
