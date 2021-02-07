a559291b1824e73efb9df9dcdd400a722af0d3a0
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2021-02-07 18:02:11 +0000

    Update standard_builtin_objects.js

--- standard_builtin_objects.js
+++ standard_builtin_objects.js
@@ -26,6 +26,7 @@ console.log(Math.sqrt(16))   // 44
 
 console.log(typeof Date)  // function
 
+
 // you can call some special methods called "static methods" directly on this builtin function 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#static_methods
 
@@ -34,6 +35,8 @@ console.log(Date.now())  // 1612720322842
 
 // However to use the 'protoytpe' functions you have to create a "Date" object and apply the function to that object
 y = new Date() 
+
+// I think the "new" indicates that this is a constructor function - https://github.com/Sher-Chowdhury/javascript_study_guide/blob/main/constructor_functions.js
 
 console.log(typeof y)    // object
 
