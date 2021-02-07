0cc25242b0c3f7209ec84363b986c56b3b026cc1
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2021-02-07 19:53:23 +0000

    Update conditions_in_return_statements.js

--- conditions_in_return_statements.js
+++ conditions_in_return_statements.js
@@ -5,9 +5,9 @@ function getNameInUpperCase(userData) {
     // So we normally use multiple condition 
     // instead of that we can use the above statement.
 }
-getNameInUpperCase(); //undefined;
-getNameInUpperCase({}); //undefined;
-getNameInUpperCase({name : "Noob master"}); // NOOB MASTER
+console.log(getNameInUpperCase())
+console.log(getNameInUpperCase({}))
+console.log(getNameInUpperCase({name : "Noob master"})) // NOOB MASTER
 
 
 // this was exmaple 5 in - https://levelup.gitconnected.com/secrets-of-logical-%EF%B8%8F-operator-in-javascript-98efbae5e8aa
