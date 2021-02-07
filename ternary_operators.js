841f2378321c153460835cd8f5011a20b87f5a36
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2021-02-07 14:05:56 +0000

    Create ternary_operators.js

---
+++ ternary_operators.js
@@ -0,0 +1,13 @@
+// ternary-operators, are if-else statetents written in a single line, e.g.:
+
+
+let price = 28 ;
+
+(price > 20) ? console.log("expensive") : console.log("cheap")
+
+// It can also be sued to set variables:
+
+
+let answer = (price > 25) ? "very expensive" : "a little expensive"
+
+console.log(answer)
