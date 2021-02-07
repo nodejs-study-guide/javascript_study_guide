e5f287a2c4a93b29d53d1fcf234fab553f3a99e1
Sher Chowdhury <Sher-Chowdhury@users.noreply.github.com>
2021-02-07 14:07:11 +0000

    Update ternary_operators.js

--- ternary_operators.js
+++ ternary_operators.js
@@ -5,6 +5,8 @@ let price = 28 ;
 
 (price > 20) ? console.log("expensive") : console.log("cheap")
 
+// the round brackets around the condition part is actually optional but is best practice to always include them for readability. 
+
 // It can also be sued to set variables:
 
 
