4b226bd83951695a916d1d96af630f67a495f717
Sher Chowdhury <sher.chowdhury@ibm.com>
2021-02-20 13:06:35 +0000

    wip

--- node/04_script_parameters.js
+++ node/04_script_parameters.js
@@ -7,6 +7,7 @@ console.log("third parameter is: " + process.argv[2])
 console.log("fourth parameter is: " + process.argv[3])
 console.log("fifth parameter is: " + process.argv[4])
 
+// all script parameters are treated as strings. 
 
 // try running:
 // node 04_script_parameters.js hello world
