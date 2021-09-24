a6042f9b9ab8d284ab22a3388256296664e9623d
Sher Chowdhury <sher.chowdhury@ibm.com>
2021-09-24 09:29:50 +0100

    wip

--- arrow_functions.js
+++ arrow_functions.js
@@ -1,100 +1,113 @@
+// Run this script from the command line like this:
+// node arrow_functions.js
+
+
+
+
+// Example A
 // A regular anonymous-function
 
-let greetings = function() {
-  return 'Hello World';
+let greetingsA = function() {
+  return 'Hello World - A';
 };
 
-let message = greetings();
-console.log(message);      // Hello World
+let messageA = greetingsA();
+console.log(messageA);      // Hello World
 
 
 
+// Example B
 // arrow function equivalent (just the first line is different)
 // the empty bracket means that there's no input parameters. 
 
-let greetings = () => {
-  return 'Hello World';
+let greetingsB = () => {
+  return 'Hello World - B';
 };
 
-let message = greetings();
-console.log(message);      // Hello World
+let messageB = greetingsB();
+console.log(messageB);      // Hello World
 
 
+// Example C
 // Since the code block only has one line, you can make it even shorter
 // by removing the curly braces and return key word
 
-let greetings = () =>  'Hello World';
+let greetingsC = () =>  'Hello World - C';
 
-let message = greetings();
-console.log(message);      // Hello World
+let messageC = greetingsC();
+console.log(messageC);      // Hello World
 
 
 
-// Now lets take a look at converting a regular-function (which has one paraemter)
+// Example D
+// Now lets take a look at converting a regular-function (which has one parameter)
 // into an arrow function
-let greet = function greetings(name) {
+let greetD = function greetingsD(name) {
   return 'Hello ' + name
 }
 
-let message = greet('John');
-console.log(message);        // Hello John 
+let messageD = greetD('JohnD');
+console.log(messageD);        // Hello JohnD
 
 
+// Example E
 // now the arrow-function equivalent
 // notice we no longer needed to create the "greet" variable
-let greetings = (name) => {
+let greetingsE = (name) => {
   return 'Hello ' + name
 }
 
-let message = greetings('John');
-console.log(message);        // Hello John 
+let messageE = greetingsE('JohnE');
+console.log(messageE);        // Hello JohnE 
 
 
 
+// Example F
 // Also when you have just one input parameter, 
 // then you can also get rid of the round brackets as well
 
 // now the arrow-function equivalent
-let greetings = name => {
+let greetingsF = name => {
   return 'Hello ' + name
 }
 
-let message = greetings('John');
-console.log(message);        // Hello John 
+let messageF = greetingsF('JohnF');
+console.log(messageF);        // Hello John 
 
-
+// Example G
 // arrow functions with 2 or more input parameters. 
 // here's the regular function
-function sum(num1, num2) {
+function sumG(num1, num2) {
   return num1 + num2;
 }
-let output = sum(5,10)
-console.log(output);        // 15
+let outputG = sumG(5,10)
+console.log(outputG);        // 15
 
 
+// Example H
 // arrow-function equivalent
-let sum = (num1, num2) => {
+let sumH = (num1, num2) => {
   return num1 + num2;
 }
-let output = sum(7,10)
-console.log(output);        // 17
+let outputH = sumH(7,10)
+console.log(outputH);        // 17
 
 
 
 
-
+// Example I
 // The "this" keyword doesn't exist in arrow-functions.
 // If you try to use it, then it ends up resolving to the 
 // global window "this" object:
 
 
-let message = {
-  name: 'John',
-  regularAnonymousFunction: function() {
+let messageI = {
+  name: 'JohnI',
+  regularAnonymousFunctionI: function() {
     console.log(this)
     console.log('Hello ' + this.name)
   },
-  arrowFunction: () => {
+  arrowFunctionI: () => {
     console.log(this)
     console.log('Hi ' + this.name)
   }
@@ -102,10 +115,6 @@ let message = {
 }
 
 
-message.regularAnonymousFunction()
-message.arrowFunction()
-
-
-
-
+messageI.regularAnonymousFunctionI()
+messageI.arrowFunctionI()
 
