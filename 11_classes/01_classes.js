2f2c1a7e8bf7d1b3b9172685225b90c4cf0917e6
Sher Chowdhury <sher.chowdhury@ibm.com>
2021-10-13 09:35:13 +0100

    wip

--- 11_classes/01_classes.js
+++ 11_classes/01_classes.js
@@ -1,15 +1,15 @@
-// Classes performs the exact same works as constructor functions (including inheritance). 
-// The only difference is that the classes has a cleaner syntax than constructors. 
+// Classes performs the exact same work as constructor functions (including inheritance). 
+// The only difference is that the classes have a cleaner syntax than constructors. 
 
 
 console.log("Example A")
 // First here's a reminder of what a simple constructor function looks like:
 
-function Person(foreName, surName, age) {
+function PersonA(foreName, surName, age) {
 	this.firstName = foreName
 	this.lastName = surName
 	this.age = age
-	this.greetings = function () { return 'hello ' + this.firstName + ' ' + this.lastName }
+	this.fullname = function () { return this.firstName + ' ' + this.lastName }
 	this.isAdult = function() { 
 		return this.age >= 18
 	}
@@ -21,8 +21,41 @@ function Person(foreName, surName, age) {
 
 // To call this constructor function, we use the "new" key word:
 
-let superman = new Person("Clark", "Kent", 40)
+let superman = new PersonA("Clark", "Kent", 40)
 
 console.log(superman)
 
 console.log(superman.greetings())
+
+
+
+console.log("Example B")
+
+// Now heres the above constructor function re-written in the form of a class. A class is defined using the 
+// "class" key word:
+
+
+class PersonB {
+
+	// Within a class, you can define a "constructor" block, using the constructor key word.
+	// This is where you set the properties for your object.
+	// The constructor's code block automatically get's executed when a new object is created.  
+	constructor(foreName, surName, age) {
+		this.firstName = foreName
+		this.lastName = surName
+		this.age = age
+	}
+
+	// To create a getter, you just use the "get" key word.:
+	get fullName() {
+		return this.firstName + ' ' + this.lastName
+	}
+
+}
+
+// noticed we use the saem "new" key word to drive the class.
+let spiderman = new PersonB("Peter", "Parker", 18)
+
+console.log(spiderman)   // PersonB { firstName: 'Peter', lastName: 'Parker', age: 18 }
+spiderman.age = 19
+console.log(spiderman)   // PersonB { firstName: 'Peter', lastName: 'Parker', age: 19 }
