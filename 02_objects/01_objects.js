3b36e48a0399427addeb3292cd9903f2c808f857
Sher-Chowdhury <sherc1980@yahoo.com>
2022-02-19 11:30:35 +0000

    wip

--- 02_objects/01_objects.js
+++ 02_objects/01_objects.js
@@ -1,5 +1,5 @@
 'use strict'
-// An object can be a user-defined composite datatype. 
+// An object can be a user-defined composite datatype.
 
 // Objects's are Golang's equivalent of "structs", i.e. a group of key-value stored inside a variable.
 // Each key-value entry is referred to either as a "property" (if value stores some data) or "method" (if value stores a function)
@@ -12,11 +12,11 @@ console.log("Example A")
 let person = {
 	name: 'John',
 	age: 32,
-	PI,      // Instead of "PI: PI", we can use this shorthand, this only works when key-Name matches with variable name. 
+	PI,      // Instead of "PI: PI", we can use this shorthand, this only works when key-Name matches with variable name.
 	greetings: function(){return `hello ${this.name}`},
 	farewell: function(){return "goodbye " + this.name },
 	myName() {return "My name is Clark Kent"},              // slightly shortand syntax
-	myAlias: ()  => {return "I'm also known as Superman"}   // example of an arrow function. 
+	myAlias: ()  => {return "I'm also known as Superman"}   // example of an arrow function.
 }
 
 
@@ -28,12 +28,12 @@ console.log(person.name)            // John
 console.log(person.age)             // 32
 console.log(person.greetings())     // hello John
 console.log(person.farewell())      // goodbye John
-console.log(person.myName()) 
-console.log(person.myAlias()) 
+console.log(person.myName())
+console.log(person.myAlias())
 
 
 // accessing a non-existient property
-console.log(person.gender)          // undefined     // noticed script doesn't fail. 
+console.log(person.gender)          // undefined     // noticed script doesn't fail.
 
 
 
@@ -56,7 +56,7 @@ console.log(person[currentAgeB])
 
 
 console.log("Example C")
-// symbol is a special data type user for storing sensitive data. 
+// symbol is a special data type user for storing sensitive data.
 
 
 let mySymbol = Symbol()
@@ -64,11 +64,27 @@ mySymbol = "secretHandShake"
 let employee = {
 	name: 'John',
 	age: 32,
-	[mySymbol]:  'myPassword' 
+	[mySymbol]:  'myPassword'
 }
 console.log(employee)
-// The only code that can access the secret data, is code that has access to the mySymbol variable. 
+// The only code that can access the secret data, is code that has access to the mySymbol variable.
 // more info, see: https://app.pluralsight.com/course-player?clipId=5667e465-8dd0-45c8-8882-5d9aa4bb4e14
+
+// Actually that is not true, because you can use the Object.getOwnPropertySymbols() function to view a symbol,
+// then edit it. See section 6.10.3 of the javascript the definitive guide (7th edition) book.
+
+// The only use for symbols, is to create properties that don't accidentally get overwritten elsewhere.
+
+// also you can pass in an optional string arguement name when creating a symbol, e.g.
+
+// let mySymbol = Symbol("an-argument")
+
+// but this string is mainly used for debugging purposes only.
+
+// note, the square brackets "[]" syntax is not something symbol specific. It is more broadly used for
+// first evaluating the contents of square-brackets first. so you can do things like [myVar] or [myFuncWithReturnValue(x)]
+// to dynamically work out the key's name.
+
 
 console.log(employee.mySymbol)    // undefined
 
