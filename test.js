b3bd50e878fb487425abd764c5e2cfa9bc39ff21
Sher-Chowdhury <sherc1980@yahoo.com>
2022-02-19 11:21:08 +0000

    wip

--- test.js
+++ test.js
@@ -1,45 +1,11 @@
 'use strict'
 
 
-// function myFunc(){
-
-// }
-
-// console.log(Object.getOwnPropertyNames(Date.prototype))
-// console.log((Date.prototype.constructor))
-
-
-// let myObject = [4,6]
-// console.log(Object.getPrototypeOf(myObject).toString)
-
-// let FruitsA = ["Apple","Banana"]
-// console.log(Object.getOwnPropertyNames(FruitsA))
-
-
-
-// let FruitsB = new Array(2)
-// FruitsB[0] = "Mango"
-// FruitsB[1] = "Orange"
-
-// console.log(Object.getOwnPropertyNames(FruitsB))
-
-// //console.log(Object.getOwnPropertyNames(Array.prototype))
-
-
-// console.log(FruitsA instanceof Array) // true
-// console.log(FruitsB instanceof Array) // true
-
-// console.log(Array.prototype)
-
-
-function Person(name,age){
-	this.name = name
-	this.age = age
+let obj1 = {
+	x: 1,
+	y: 2,
+	//toString: function() {return `(${this.x}, ${this.y})`}
 }
 
-Person.prototype.title = "Mr"
-
-
-let pete = new Person("Pete", 18)
-
-console.log(Person.prototype)
+console.log(String(obj1))
+console.log(obj1.toString())
