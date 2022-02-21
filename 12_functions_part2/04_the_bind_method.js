0e649026bf83f96dfaf297626e63e0e26c870bbe
Sher-Chowdhury <sherc1980@yahoo.com>
2022-02-21 17:13:37 +0000

    wip

--- 12_functions_part2/04_the_bind_method.js
+++ 12_functions_part2/04_the_bind_method.js
@@ -1,6 +1,6 @@
 /*
-Let's say we have to objects (structs), person1 and person2. 
-One of person1's property is a function (aka method), however person2 doesn't have 
+Let's say we have two objects, person1 and person2.
+One of person1's property is a function (aka method), however person2 doesn't have
 this property+function (aka method).
 
 However you can still apply this person1's method to person2 using the "bind()" builtin function
@@ -19,10 +19,13 @@ let person2 = {
 
 // The "bind" method creates a copy of the "getName" function and stores it as a function express, called "result.
 // Also this new function's internal "this" object is storing the content of person2 object.
-result =  person1.getName.bind(person2) 
+result =  person1.getName.bind(person2)
 
-// notice this is a function. This function has 'this' pre-saved to 
-// the person2 object in this new function. 
+// notice this is a function. This function has 'this' pre-saved to
+// the person2 object in this new function.
 console.log(result) // [Function: bound getName]
 
+console.log(result.toString()) // function () { [native code] }
+
+console.log(   result()   ); // John is undefined
 console.log(   result(23)   ); // John is 23
