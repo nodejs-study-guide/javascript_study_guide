68635089c329ae122bd9e8202fe4fd3476eb8c73
Sher-Chowdhury <sherc1980@yahoo.com>
2022-02-21 11:11:44 +0000

    wip

--- 12_functions_part2/08_functions_are_objects.js
+++ 12_functions_part2/08_functions_are_objects.js
@@ -1,15 +1,20 @@
 'use strict'
 
+// In javascript, a function is actually a type of object.
+
+
 function greetings(){
 	console.log("hello")
 }
 
 console.log(greetings) // [Function: greetings]
 
-// to view the content of this function
+// to view the content of this object.
 console.log(greetings.toString())
 // this outputs:
 
 // function greetings(){
 // 	console.log("hello")
 // }
+
+
