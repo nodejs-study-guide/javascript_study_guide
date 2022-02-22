aa0fddcf306b9dde38904e7ee172c08a9b9b4c2f
Sher-Chowdhury <sherc1980@yahoo.com>
2022-02-22 17:47:30 +0000

    wip

---
+++ 12_functions_part2/04a_partial_application_using_bind.js
@@ -0,0 +1,27 @@
+'use strict'
+/*
+
+The bind() can also be used to provide hard-code arguments for input paramters, which cannot be chnaged.
+*/
+
+
+let person1 = {
+  name: 'Mary',
+  getName: function(age){
+    return this.name + ' is ' + age
+  },
+}
+
+
+let person2 = {
+  name: 'John',
+}
+
+
+// Here we're hard-coding the age to 25.
+let result =  person1.getName.bind(person2, 25)
+
+console.log(   result()   ) // John is 25
+
+// We try to pass in an argument of 30, but that get's ignored.
+console.log(   result(30)   ) // John is 25
