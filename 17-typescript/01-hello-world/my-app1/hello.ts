7a5fa20876d27fa421f52b020be0286f25eeea1c
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-12-07 18:34:32 +0000

    wip

--- 17-typescript/01-hello-world/my-app1/hello.ts
+++ 17-typescript/01-hello-world/my-app1/hello.ts
@@ -1,3 +1,8 @@
-"use strict";
-// Greets the world.
-console.log("Hello world!");
+
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person: string, date: string) {
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet("Brendan", "monday");
